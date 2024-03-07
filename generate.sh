7be96da7d46874cc9b848f0cf0d518afd24b94b8
9551-Dev <9551dev@gmail.com>
2024-03-08 00:13:19 +0100

    update radioburza sh file config location

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # radioburzy
-python3 ./gallery_generator.py config/radioburzy/2024_leden.ini
+python3 ./gallery_generator.py config/radioburza/leden_2024.ini
 
 # laboratoř EE
 python3 ./gallery_generator.py config/lab/storage_2024_03_07.ini
