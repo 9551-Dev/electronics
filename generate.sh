4b02b5063638629265a9c9c6d9c60f1574a630ac
9551-Dev <9551dev@gmail.com>
2024-03-08 01:03:11 +0100

    add toob

--- generate.sh
+++ generate.sh
@@ -5,3 +5,6 @@ python3 ./gallery_generator.py config/radioburza/leden_2024.ini
 
 # laboratoř EE
 python3 ./gallery_generator.py config/lab/storage_2024_03_07.ini
+
+# elektronky
+python3 ./gallery_generator.py config/tubes/gu81m.ini
