7d6645ced9e38b6b58ec7f975965b0dee37f5e8e
9551-Dev <9551dev@gmail.com>
2024-03-11 08:43:17 +0100

    add radioburza_brezen_2024

--- generate.sh
+++ generate.sh
@@ -2,6 +2,7 @@
 
 # radioburzy
 python3 ./gallery_generator.py config/radioburza/leden_2024.ini
+python3 ./gallery_generator.py config/radioburza/brezen_2024.ini
 
 # laboratoř EE
 python3 ./gallery_generator.py config/lab/storage_2024_03_07.ini
