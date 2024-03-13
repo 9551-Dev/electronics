152a8e5e88402f57f1f686eee7676b7e2bef2dc7
9551-Dev <9551dev@gmail.com>
2024-03-13 17:30:47 +0100

    added more tube pics and a new category

--- generate.sh
+++ generate.sh
@@ -9,3 +9,4 @@ python3 ./gallery_generator.py config/lab/storage_2024_03_07.ini
 
 # elektronky
 python3 ./gallery_generator.py config/tubes/gu81m.ini
+python3 ./gallery_generator.py config/tubes/general.ini
