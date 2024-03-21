3d1301eb32863a911eb796b7a3fc782f51fad96d
9551-Dev <9551-Dev@users.noreply.github.com>
2024-03-21 22:35:02 +0000

    Build for 32c70914c1116210c4849c254957e9d7df6a094d

---
+++ generate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# radioburzy
+python3 ./gallery_generator.py config/radioburza/leden_2024.ini
+python3 ./gallery_generator.py config/radioburza/brezen_2024.ini
+
+# laboratoř EE
+python3 ./gallery_generator.py config/lab/storage_2024_03_07.ini
+
+# elektronky
+python3 ./gallery_generator.py config/tubes/gu81m.ini
+python3 ./gallery_generator.py config/tubes/general.ini
+
+# projects
+python3 ./gallery_generator.py config/projects/sstc_v1.ini
