ab6a21e82eaa7261cf9662b07f01832909450f65
9551-Dev <9551dev@gmail.com>
2024-06-28 13:26:24 +0700

    more future proof generate.sh file

--- generate.sh
+++ generate.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-# radioburzy
-python3 ./gallery_generator.py ./assets/common_config.ini config/events/radioburza/leden_2024.ini
-python3 ./gallery_generator.py ./assets/common_config.ini config/events/radioburza/brezen_2024.ini
-python3 ./gallery_generator.py ./assets/common_config.ini config/events/radioburza/kveten_2024.ini
+COMMON_CONFIG="./assets/common_config.ini"
 
-# laboratoř EE
-python3 ./gallery_generator.py ./assets/common_config.ini config/lab/storage_2024_03_07.ini
+declare -a GALLERY_CONFIGS=(
+    "config/events/radioburza/leden_2024.ini"
+    "config/events/radioburza/brezen_2024.ini"
+    "config/events/radioburza/kveten_2024.ini"
 
-# elektronky
-python3 ./gallery_generator.py ./assets/common_config.ini config/tubes/gu81m.ini
-python3 ./gallery_generator.py ./assets/common_config.ini config/tubes/general.ini
+    "config/lab/storage_2024_03_07.ini"
 
-# projects
-python3 ./gallery_generator.py ./assets/common_config.ini config/projects/sstc_v1.ini
-python3 ./gallery_generator.py ./assets/common_config.ini config/projects/hfvttc_v1.ini
+    "config/tubes/gu81m.ini"
+    "config/tubes/general.ini"
 
-# misc
-python3 ./gallery_generator.py ./assets/common_config.ini config/misc/pat_a_mat.ini
+    "config/projects/sstc_v1.ini"
+    "config/projects/hfvttc_v1.ini"
+
+    "config/misc/pat_a_mat.ini"
+)
+
+for config in "${GALLERY_CONFIGS[@]}"; do
+    python3 ./gallery_generator.py $COMMON_CONFIG $config
+done
